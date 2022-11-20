aed7b2c20330987605ea09f4e8aebdb446c35d7c
Sirius Black <mariia.beliaev@gmail.com>
2022-11-20 23:20:48 +0300

    added gallery

---
+++ gallery/index.js
@@ -0,0 +1,9 @@
+function goBack() {
+  let photo = document.getElementById("photo1");
+  photo.src = "./assets/black_mushroom.jpg";
+}
+
+function goForward() {
+  let photo = document.getElementById("photo1");
+  photo.src = "./assets/white_mushroom.jpg";
+}
