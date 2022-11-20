82d5dba03e0fce3d0ecc3608f182d5d2009f9d17
Sirius Black <mariia.beliaev@gmail.com>
2022-11-20 22:16:42 +0300

    added calculator

---
+++ calcW14/index.js
@@ -0,0 +1,32 @@
+let n1;
+let n2;
+
+let result = document.getElementById("result");
+
+function add() {
+  n1 = document.getElementById("number1").value;
+  n2 = document.getElementById("number2").value;
+
+  result.innerHTML = +n1 + +n2;
+}
+
+function subtract() {
+  n1 = document.getElementById("number1").value;
+  n2 = document.getElementById("number2").value;
+
+  result.innerHTML = +n1 - +n2;
+}
+
+function multiply() {
+  n1 = document.getElementById("number1").value;
+  n2 = document.getElementById("number2").value;
+
+  result.innerHTML = +n1 * +n2;
+}
+
+function divide() {
+  n1 = document.getElementById("number1").value;
+  n2 = document.getElementById("number2").value;
+
+  result.innerHTML = +n1 / +n2;
+}
